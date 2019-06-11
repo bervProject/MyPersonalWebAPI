883805a70c5cc665a5a0f99e82f9daa54b3c30a0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-06-11 16:22:31 +0700

    Update env and connectionString

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres');
+  const connectionString = process.env.CONNECTION_STRING || app.get('postgres');
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
     logging: false,
