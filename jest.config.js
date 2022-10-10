11af5f041a26e59fd684e3a7ae7ee6ceccdf741f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-10-10 19:41:47 +0700

    Update deps and test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-    },
-  },
   collectCoverage: true,
   collectCoverageFrom: ["**/src/*.{ts,tsx}"],
   coverageDirectory: "./coverage/",
